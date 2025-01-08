6631bcd28a4dae7f91175ce3cc59b11bd077c8f7
Felipe Daniel Juarez Alvarez <spider_spainfel@hotmail.com>
2025-01-08 16:04:11 -0600

    feat: Base HTTP Service

--- src/app/products/features/products-list/products-list.component.ts
+++ src/app/products/features/products-list/products-list.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ProductsService } from '../../data-access/products.service';
 
 @Component({
   selector: 'app-products-list',
+  standalone: true,
   imports: [],
   templateUrl: './products-list.component.html',
-  styles: ``
+  styles: ``,
+  providers: [ProductsService],
 })
 export default class ProductsListComponent {
+  private productsService = inject(ProductsService);
 
+  constructor() {
+    this.productsService.getProducts().subscribe((products) => {
+      console.log(products);
+    });
+  }
 }
