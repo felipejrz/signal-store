d7bad5c53a0731aec36269d671210f4eb3be2e1d
Felipe Daniel Juarez Alvarez <spider_spainfel@hotmail.com>
2025-01-08 18:31:23 -0600

    Product Card

--- src/app/products/features/products-list/products-list.component.ts
+++ src/app/products/features/products-list/products-list.component.ts
@@ -1,15 +1,16 @@
 import { Component, inject } from '@angular/core';
 import { ProductsService } from '../../data-access/products.service';
 import { ProductsStateService } from '../../data-access/products-state.service';
+import { ProductCardComponent } from '../../ui/product-card/product-card.component';
 
 @Component({
   selector: 'app-products-list',
   standalone: true,
-  imports: [],
+  imports: [ProductCardComponent],
   templateUrl: './products-list.component.html',
   styles: ``,
   providers: [ProductsStateService, ProductsService],
 })
 export default class ProductsListComponent {
-  productsState = inject(ProductsStateService)
+  productsState = inject(ProductsStateService);
 }
